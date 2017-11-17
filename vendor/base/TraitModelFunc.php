554b485a7a8e8208d2ed9bc237191305ab9e0517
Mick911 <mick911@mail.ru>
2017-11-17 18:31:09 +0300

    my changes17112017

---
+++ vendor/base/TraitModelFunc.php
@@ -0,0 +1,44 @@
+<?php
+
+
+trait TraitModelFunc
+{
+    /**
+     * function set - set private object property
+     * @$prop_name string - property name
+     * @$prop_value - property value
+     * @author terentyev.m
+     */
+
+
+    public function set($prop_name, $prop_value) {
+        if (property_exists($this, $prop_name)) {
+			$this->$prop_name = $prop_value;
+		}
+    }
+
+    /**
+     * function set - set private object property
+     * @$prop_name - property name
+     * return peroperty value, if property doesn't extists then return null
+     * @author terentyev.m
+     */
+    public function get($prop_name) {
+        if (property_exists($this, $prop_name)) {
+			return $this->$prop_name;
+        }
+        
+        return null;
+    }
+
+    public function getMapping() {
+        
+        if (isset($this->mapping)){
+            return $mapping;
+        }
+        
+        return [];
+    }
+
+    
+}
