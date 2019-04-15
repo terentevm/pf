9f9b694fd4b96a6a5aa675d4ccba59d30f84099a
OSTEC\terentyev.m <mick911@mail.ru>
2019-04-15 17:50:32 +0300

    getting rates from ecb

--- tm/rates/ECBLoader.php
+++ tm/rates/ECBLoader.php
@@ -10,10 +10,64 @@ namespace tm\rates;
 
 
 class ECBLoader implements IRatesLoader
-{
-    public function load(string $currencyCode, string $date1, string $date2): RateData
+{private $currencyCode;
+    private $date1;
+    private $date2;
+
+    private $url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip';
+
+    private $data = [];
+
+    public function load(string $currencyCode, string $date1, string $date2)
     {
-        // TODO: Implement load() method.
+        if (empty($this->data)) {
+            $this->data = $this->makeRequest();
+        }
     }
 
+    private function makeRequest()
+    {
+        $tempFile = tempnam ( sys_get_temp_dir(), 'rates');
+        
+        if ($tempFile === false) {
+            throw new Exception("Can't create temp file! ", 4);
+        }
+        $stream_context = stream_context_create([ 'ssl' => [
+            'verify_peer'       => false,
+            'verify_peer_name'  => false,
+            'allow_self_signed' => true,
+            'verify_depth'      => 0 ]]);
+
+        file_put_contents($tempFile, file_get_contents($this->url, null, $stream_context));
+        
+        if (($handle = fopen('zip://' . $tempFile . '#eurofxref-hist.csv', 'r')) !== FALSE) {
+            
+            $header = [];
+            $index = 0;
+
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if ($index === 0) {
+                    $header = $data;
+                    $index++;
+                    continue;
+                }
+
+                $rates = [];
+
+                for ($i = 1; $i< count($header); $i++){
+                    $rate = floatval($data[$i]);
+                    $rates[$header[$i]] = $rate;
+                }
+
+                $this->data[$data[0]] = $rates;
+                $index++;
+            }
+
+            fclose($handle);
+        }    
+
+        unlink($tempFile);
+
+        print_r($this->data);
+    }
 }
